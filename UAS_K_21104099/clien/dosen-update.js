898352c26e3bf3924603990a2fc5ed5b620d53f5
janwargustowi <50947634+janwargustowi@users.noreply.github.com>
2022-06-27 23:40:36 +0800

    Update dosen-update.js

--- UAS_K_21104099/clien/dosen-update.js
+++ UAS_K_21104099/clien/dosen-update.js
@@ -11,7 +11,7 @@ function dosenupdate(){
     //console.log(dta)
 
     xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("POST","http://localhost/febri/server/dtadosen-insertdata.php");
+    xmlhttp.open("POST","http://localhost/UAS_K_21104099/server/dtadosen-insertdata.php");
     xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
     xmlhttp.send(dta);
 
